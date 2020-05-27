3707baafd2e26837e3074e5a256d9293625babe9
rk110047 <rk110047@gmail.com>
2020-05-27 18:10:46 +0530

    54

--- shopkeeper/views.py
+++ shopkeeper/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import ShopProfile
+from django.db.models import Q
 from .serializer import ShopProfileCreateSerializer,ShopsSerializer
 from rest_framework.authentication import SessionAuthentication
 
@@ -18,8 +19,15 @@ class ShopsAPIView(generics.ListAPIView):
 	authentication_classes  =   []
 	serializer_class        =   ShopsSerializer
 	permission_classes      =   []
-	def get_queryset(self,*args,**kwargs):
-		shops = ShopProfile.objects.filter(active=True)
-		return shops
+
+	def get_queryset(self):
+		request 	= 	self.request
+		queryset    =	ShopProfile.objects.filter(active=True)
+		query       =   request.GET.get('q')
+		print(query)
+		if query is not None:
+			queryset 		= 	queryset.filter(Q(shop_name__icontains=query))
+			# return shops
+		return queryset
 
 
