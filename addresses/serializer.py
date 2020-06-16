03b2cbc9db96ffa560074589e740b8705b0fdde9
rk110047 <rk110047@gmail.com>
2020-06-16 23:31:44 +0530

    walah

--- addresses/serializer.py
+++ addresses/serializer.py
@@ -5,4 +5,4 @@ class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model  =    Address
         fields =    '__all__'
-        read_only_fields='billingprofile','address_type'
+        read_only_fields=['billingprofile','address_type']
