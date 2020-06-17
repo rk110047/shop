3211a590b3ef85996ea47fbbf3dde22ec1b769f4
rk110047 <rk110047@gmail.com>
2020-06-18 01:37:22 +0530

    57547

--- customer/models.py
+++ customer/models.py
@@ -7,15 +7,15 @@ class CustomerProfile(models.Model):
     User                =    models.OneToOneField(User,on_delete=models.CASCADE)
     profile_image       =    models.FileField(upload_to='profile_pic/',null=True)
     first_name          =    models.CharField(max_length=20)
-    middle_name         =    models.CharField(max_length=20,null=True,blank=True)
-    last_name           =    models.CharField(max_length=20,null=True,blank=True)
+    # middle_name         =    models.CharField(max_length=20,null=True,blank=True)
+    # last_name           =    models.CharField(max_length=20,null=True,blank=True)
     email               =    models.EmailField()
     phone_Number        =    models.CharField(max_length=120,null=True,blank=True)
-    address_line_1      =    models.CharField(max_length=120)
-    town_city           =    models.CharField(max_length=120)
-    state               =    models.CharField(max_length=120)
-    country             =    models.CharField(max_length=120)
-    zip_code            =    models.CharField(max_length=20,null=True,blank=True)
+    # address_line_1      =    models.CharField(max_length=120,null=True,blank=True)
+    # town_city           =    models.CharField(max_length=120,null=True,blank=True)
+    # state               =    models.CharField(max_length=120,null=True,blank=True)
+    # country             =    models.CharField(max_length=120,null=True,blank=True)
+    # zip_code            =    models.CharField(max_length=20,null=True,blank=True)
 
 
 
