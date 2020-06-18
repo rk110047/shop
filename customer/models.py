a5a7bc91a9d05c486e526c3db95710440f600563
rk110047 <rk110047@gmail.com>
2020-06-18 17:22:36 +0530

    xkhghfc

--- customer/models.py
+++ customer/models.py
@@ -8,14 +8,14 @@ class CustomerProfile(models.Model):
     # profile_image       =    models.FileField(upload_to='profile_pic/',null=True)
     first_name          =    models.CharField(max_length=20)
     # middle_name         =    models.CharField(max_length=20,null=True,blank=True)
-    # last_name           =    models.CharField(max_length=20,null=True,blank=True)
+    last_name           =    models.CharField(max_length=20,null=True,blank=True)
     email               =    models.EmailField()
     phone_Number        =    models.CharField(max_length=120,null=True,blank=True)
     # address_line_1      =    models.CharField(max_length=120,null=True,blank=True)
-    # town_city           =    models.CharField(max_length=120,null=True,blank=True)
+    town_city           =    models.CharField(max_length=120,null=True,blank=True)
     # state               =    models.CharField(max_length=120,null=True,blank=True)
     # country             =    models.CharField(max_length=120,null=True,blank=True)
-    # zip_code            =    models.CharField(max_length=20,null=True,blank=True)
+    zip_code            =    models.CharField(max_length=20,null=True,blank=True)
 
 
 
